3a75e310a7a9277ec99e8d6e91213cbb6ec57265
georgezywang <georgezywang@outlook.com>
2020-12-19 17:11:41 -0500

    add CLI for parser

--- driver.py
+++ driver.py
@@ -1,5 +1,7 @@
 import MaskCoco
 import os
+import json
+import argparse
 
 def fileDic(path):
     imgList = []
@@ -12,8 +14,26 @@ def fileDic(path):
             imgList.append(img)
     return imgList
 
-catDic = {"wave": 0}
-imgList = fileDic("images")
-myParser = MaskCoco.MaskParser(catDic, imgList, inPath = "images",\
-                               outPath="images/dat.json", useImgID = True)
-myParser.saveJson()
+def readDict(path):
+    with open(path, 'r') as file:
+        catDic = json.load(file)
+    return catDic
+
+def checkArgs(args):
+    assert os.path.exists(args.inpath), "invalid input path"
+    assert os.path.exists(args.catpath), "invalid category dictionary path"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    fileDir = os.path.dirname(__file__)
+    parser.add_argument("-i", "--inpath", help = "directory of input images")
+    parser.add_argument("-o", "--outpath", help = "coco json output file path", required = False, default = os.path.join(fileDir, "data.json"))
+    parser.add_argument("-c", "--catpath", help = "path of category dictionary json file")
+    parser.add_argument("-d", "--imageid", help = "use image id", action='store_true', required = False)
+    args = parser.parse_args()
+    checkArgs(args)
+
+    catDic = readDict(args.catpath)
+    imgList = fileDic(args.inpath)
+    myParser = MaskCoco.MaskParser(catDic, imgList, inPath = args.inpath, outPath=args.outpath, useImgID = False)
+    myParser.saveJson()
